0d15e24fbdb9c615e8ad5763ed9620627b9f7488
salvador Munoz <info@luzmatel.com>
2026-01-09 16:32:58 +0000

    feat: añadir selector de símbolos precargados con 13 categorías

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open('iconos-pasteles-v1').then(cache => {
+      return cache.match(event.request).then(response => {
+        return response || fetch(event.request).then(networkResponse => {
+          if (event.request.method === 'GET' && networkResponse.ok && event.request.url.startsWith(self.location.origin)) {
+            cache.put(event.request, networkResponse.clone());
+          }
+          return networkResponse;
+        });
+      });
+    })
+  );
+});
